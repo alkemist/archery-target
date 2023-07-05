0cea42b78f665adeab48f82b36a5e49463bd8f7a
Jérémy ACHAIN <achain.jeremy@gmail.com>
2023-07-05 11:14:07 +0200

    Add env default files

---
+++ src/environments/environment.interface.ts
@@ -0,0 +1,14 @@
+export interface Environment {
+  APP_NAME: string;
+  APP_VERSION: string;
+  APP_LOCAL: boolean;
+  APP_DEBUG: boolean;
+  APP_OFFLINE: boolean;
+  FIREBASE_API_KEY: string;
+  FIREBASE_APP_ID: string;
+  FIREBASE_PROJECT_ID: string;
+  FIREBASE_MESSAGING_SENDER_ID: string;
+  FIREBASE_STORAGE_BUCKET: string;
+  FIREBASE_AUTH_DOMAIN: string;
+  GOOGLE_CLOUD_OPERATIONS_API_KEY: string;
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+import {Environment} from "./environment.interface";
+
+export const environment: Environment =
+  {
+    APP_NAME: "",
+    APP_VERSION: "",
+    APP_LOCAL: true,
+    APP_DEBUG: true,
+    APP_OFFLINE: false,
+    FIREBASE_PROJECT_ID: "",
+    FIREBASE_STORAGE_BUCKET: "",
+    FIREBASE_APP_ID: "",
+    FIREBASE_API_KEY: "",
+    FIREBASE_AUTH_DOMAIN: "",
+    FIREBASE_MESSAGING_SENDER_ID: "",
+    GOOGLE_CLOUD_OPERATIONS_API_KEY: "",
+  };
